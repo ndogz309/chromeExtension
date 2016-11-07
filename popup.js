cfaa723f68367fb2f2c9829bb9c98ac32e600b41
N Dawg <nicole.atack+1@gmail.com>
2016-11-07 15:55:35 -0800

    add save link functionality to pop up (using auth tokens)

--- popup.js
+++ popup.js
@@ -1,9 +1,83 @@
+document.getElementById('log-in').onclick = login;
+document.getElementById('get-token').onclick = getToken;
+document.getElementById('delete-token').onclick = deleteToken;
+document.getElementById('save-link').onclick = saveLink;
+
+
 onload = isSignedIn;
+var auth_token;
+
+function saveLink() {
+
+var tabUrl;
+
+ chrome.tabs.getSelected(null, function(tab) {
+       //var tabId = tab.id;
+        tabUrl = tab.url;
+
+       console.log(tab.url);
+
+
+var apiUrl="http://localhost:3000/api/v1/links";
+
+ var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "POST", apiUrl, false ); // false for synchronous request
+    xmlHttp.setRequestHeader("Authorization", auth_token);
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+  //  xmlHttp.send(link);
+  xmlHttp.send(JSON.stringify({link:{url:tabUrl}}));
+
+
+console.log(xmlHttp.responseText);
+
+
+if (xmlHttp.status==201){
+document.getElementById('login').textContent = "link saved";
+
+
+
+}else{
+
+	document.getElementById('login').textContent = "something went wrong, couldnt save url";
+}
+
+
+
+
+
+      });
+
+
+
+
+
+
+// chrome.storage.sync.get('token',function(item){
+
+// console.log("heeeeeeere-------")
+// console.log(item["token"]);
+// token=item["token"];
+
+//    });
+
+
+
+
+
+    //return xmlHttp.responseText;
+
+      }
+
+
+
+
+
 
 function isSignedIn(){
 chrome.storage.sync.get('token',function(item){
 	if(item["token"]){
 		console.log(item["token"]);
+		auth_token=item["token"];
 		console.log("is logged in");
 
 	document.body.classList.add('signed-in');
@@ -18,9 +92,6 @@ chrome.storage.sync.get('token',function(item){
 		}
 	});
 }
-
-
-
 
 
 
@@ -67,6 +138,7 @@ if (xmlHttp.status==200){
 	var token=user["auth_token"];
 	console.log(token);
 	console.log("signed in 200");
+	document.getElementById('login').textContent = "logged in";
 
 saveToken(token);
 
@@ -89,9 +161,7 @@ saveToken(token);
 
 
 
-document.getElementById('log-in').onclick = login;
-document.getElementById('get-token').onclick = getToken;
-document.getElementById('delete-token').onclick = deleteToken;
+
 
 function getToken() {
 
